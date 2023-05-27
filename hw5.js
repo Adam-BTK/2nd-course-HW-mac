c03b087573c47fe48181c200bad3ad52e2f1dd99
Адам Батукаев <mcxchr@yandex.ru>
2023-05-27 03:07:31 +0200

    finished with 2.6 hw (arrays)

---
+++ hw5.js
@@ -0,0 +1,130 @@
+// let arr = [1, 5, 4, 10, 0, 3];
+
+// for (const el of arr) {
+//   console.log(el);
+//   if (el === 10) {
+//     break;
+//   }
+// }
+  
+// Задание 1
+
+// let arr = [1, 5, 4, 10, 0, 3];
+// arr = arr.indexOf(4);
+// console.log(arr);
+
+// Задание 2
+
+// let arr = [1, 3, 5, 10, 20];
+
+// let newArr = arr.join(' ');
+// console.log(newArr);
+
+// Задание 3
+
+
+// const arr = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+// const outputArr = [];
+
+// for (const inArr of arr) {
+//   const newArray = [];
+  
+//   for (const element of inArr) {
+//     newArray.push(element);
+//   }
+  
+//   outputArr.push(newArray);
+// }
+
+// console.log(outputArr);
+
+
+
+// Задание 4
+
+// let arr = [1, 1, 1];
+// arr = arr.concat(2,2,2);
+// console.log(arr);
+
+// Задание 5
+
+// let arr = [9, 8, 7, 'a', 6, 5];
+
+// arr.sort();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// Задание 6
+
+// let arr = [9, 8, 7, 6, 5];
+// let userInput = Number(prompt('Введите число'));
+// let found = false;
+
+// for (const el of arr) {
+//   if (userInput === el) {
+//     found = true;
+//     break;
+//   }
+// }
+
+// console.log(found);
+
+// Задание 7
+
+
+// let arr = ['abcdef'];
+// arr[0] = arr[0].split('');
+// arr[0].reverse();
+// arr[0] = arr[0].join('');
+
+// console.log(arr);
+
+// Задание 8
+
+
+// let arr = [[1, 2, 3,],[4, 5, 6]];
+// let newArr = [].concat(...arr);
+// console.log(newArr);
+
+// Задание 9
+
+// let arr = [1, 2, 3, 4, 5, 6, 9, 3, 5];
+// console.log(arr);
+// for (let i = 0; i < arr.length - 1; i++) {
+//     arr[i] += arr[i + 1];
+//   }
+  
+// console.log(arr);
+
+// Задание 10
+
+// const arr = [1, 2, 3, 4, 5];
+
+// let result = arr.map(item => (item ** 2));
+// console.log(result);
+
+
+// Задание 11
+
+
+// const getLengthWords = source => source.map(item => item.length);
+
+// console.log(getLengthWords(['слово', '', 'слог', 'длинное предложение', 'буква']));
+
+// Задание 12
+
+
+// let arr = [-2, 5, 1, -5, -1, 1, 3, 4, -1];
+// let arrNew = [];
+// arrNew = arr.filter(function(elem) {
+// 	if (elem <= 0) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// });
+
+// console.log(arrNew);
+
+// Задание 13
--- test.js
+++ test.js
@@ -492,3 +492,245 @@
 // }
 
 // printSequence();
+
+
+// Massives 2.6
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// for (let i = 0; i < numbs.length; i++) {
+// 	console.log(numbs[i]);
+// }
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// for (let i = 0; i < numbs.length - 1; i++) {
+//   numbs[i + 1] += numbs[i];
+// }
+
+// console.log(numbs);
+
+// for (let i = 0; i < numbs.length - 1; i++) {
+//   numbs[i] += numbs[i + 1];
+// }
+
+// console.log(numbs);
+
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// numbs.forEach((el) => {
+// 	console.log(el);
+// });
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// numbs.forEach((el, index) => {
+// 	console.log(`${index}: ${el}`);
+// });
+
+// const arr = [3,5,8,2,9,10,11,15,14,16,22];
+
+// const even = arr.filter(el => el % 2 === 0);
+
+// console.log(even);
+
+// const salary = [45000, 39000, 105000, 76000, 77000];
+
+// const tax = salary.map(el => el * 0.13);
+// console.log(tax);
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+// matrix.forEach(function(row) {
+//   row.forEach(function(element) {
+//     console.log(element);
+//   });
+// });
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+// const multipliedMatrix = matrix.map(function(row) {
+//   return row.map(function(element) {
+//     return element * 2;
+//   });
+// });
+
+// console.log(multipliedMatrix);
+
+// function funcSum(...arrSum) {
+//   let sum = 0;
+//   for (let item of arrSum) {
+//   sum += item;
+//   }
+//   return sum;
+// }
+
+// console.log(funcSum(3, 5, 7));
+
+// let arrSum = [3, 4, 5, 6]
+
+// const sum = (a, b, ...rest) => {
+//   let result = a + b;
+//   console.log(result);
+//   console.log(rest);
+// }
+
+// console.log(sum(...arrSum));
+
+
+// let arr = [1, 5, 4, 10, 0, 3];
+
+// for (const el of arr) {
+//   console.log(el);
+//   if (el === 10) {
+//     break;
+//   }
+// }
+  
+// Задание 1
+
+// let arr = [1, 5, 4, 10, 0, 3];
+// arr = arr.indexOf(4);
+// console.log(arr);
+
+// Задание 2
+
+// let arr = [1, 3, 5, 10, 20];
+
+// let newArr = arr.join(' ');
+// console.log(newArr);
+
+// Задание 3
+
+
+// const arr = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+// const outputArr = [];
+
+// for (const inArr of arr) {
+//   const newArray = [];
+  
+//   for (const element of inArr) {
+//     newArray.push(element);
+//   }
+  
+//   outputArr.push(newArray);
+// }
+
+// console.log(outputArr);
+
+
+// let arr = [];
+
+// for (let i = 0; i < 3; i++) {
+// 	arr[i] = [];
+	
+// 	for (let j = 0; j < 3; j++) {
+// 		arr[i].push(1);
+// 	}
+// }
+
+// console.log(arr);
+
+// Задание 4
+
+// let arr = [1, 1, 1];
+// arr = arr.concat(2,2,2);
+// console.log(arr);
+
+// Задание 5
+
+// let arr = [9, 8, 7, 'a', 6, 5];
+
+// arr.sort();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// Задание 6
+
+// let arr = [9, 8, 7, 6, 5];
+// let userInput = Number(prompt('Введите число'));
+// let found = false;
+
+// for (const el of arr) {
+//   if (userInput === el) {
+//     found = true;
+//     break;
+//   }
+// }
+
+// console.log(found);
+
+// Задание 7
+
+
+// let arr = ['abcdef'];
+// arr[0] = arr[0].split('');
+// arr[0].reverse();
+// arr[0] = arr[0].join('');
+
+// console.log(arr);
+
+// Задание 8
+
+
+// let arr = [[1, 2, 3,],[4, 5, 6]];
+// let newArr = [].concat(...arr);
+// console.log(newArr);
+
+// Задание 9
+
+// let arr = [1, 2, 3, 4, 5, 6, 9, 3, 5];
+// console.log(arr);
+// for (let i = 0; i < arr.length - 1; i++) {
+//     arr[i] += arr[i + 1];
+//   }
+  
+// console.log(arr);
+
+// Задание 10
+
+// const arr = [1, 2, 3, 4, 5];
+
+// let result = arr.map(item => (item ** 2));
+// console.log(result);
+
+// Вариант 2
+// for (const element of arr) {
+//   // Code to be executed for each element
+//   console.log(element** 2);
+// }
+
+// Задание 11
+
+
+// const getLengthWords = source => source.map(item => item.length);
+
+// console.log(getLengthWords(['слово', '', 'слог', 'длинное предложение', 'буква']));
+
+// Задание 12
+
+
+
+// let arr = [-2, 5, 1, -5, -1, 1, 3, 4, -1];
+// let arrNew = [];
+// arrNew = arr.filter(function(elem) {
+// 	if (elem <= 0) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// });
+
+// console.log(arrNew);
+
+// Задание 13
